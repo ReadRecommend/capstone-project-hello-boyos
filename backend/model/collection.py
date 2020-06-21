3e5911efcc82991773e4b4845b7d902f58e85826
Felix Lonergan <felix.lonergan@gmail.com>
2020-06-21 22:04:31 +1000

    Rename user to reader

--- backend/model/collection.py
+++ backend/model/collection.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.associationproxy import association_proxy
 class Collection(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    reader_id = db.Column(db.Integer, db.ForeignKey("reader.id"), nullable=False)
 
     books = db.relationship("in_collections", back_populates="books")
     books = association_proxy("in_collections", "book")
--- backend/model/review.py
+++ backend/model/review.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 
 class Review(db.Model):
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
+    reader_id = db.Column(db.Integer, db.ForeignKey("reader.id"), primary_key=True)
     book_id = db.Column(db.String, primary_key=True)
 
     creation_date = db.Column(db.DateTime, default=datetime.utcnow())
@@ -12,4 +12,4 @@ class Review(db.Model):
     score = db.Column(db.Integer)
 
     def __repr__(self):
-        return f"<Review(user_id='{self.user_id}', book_isbn='{self.book_id}', creation_date='{self.creation_date}', score='{self.score}')>"
+        return f"<Review(reader_id='{self.reader_id}', book_isbn='{self.book_id}', creation_date='{self.creation_date}', score='{self.score}')>"
