0aeb60b81b44c35cbc0663c34af2e3e8a463c0d0
Felix Lonergan <felix.lonergan@gmail.com>
2020-06-21 20:39:47 +1000

    Fix circular import

--- backend/__init__.py
+++ backend/__init__.py
@@ -5,7 +5,6 @@ from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
 from backend.config import Config
-from backend.database.json_to_db import json_to_db
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,6 +14,8 @@ ma = Marshmallow(app)
 
 
 # Refresh database every time
+from backend.database.json_to_db import json_to_db
+
 db.drop_all()
 db.create_all()
 json_to_db(os.path.abspath(os.environ.get("INITIAL_DATA", "books.json")))
