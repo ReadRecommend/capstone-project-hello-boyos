4fd42e872599a4035ed693fd00d209079abb2e56
Joshua Derbe <joshuaderbe@hotmail.com>
2020-06-26 21:32:34 +1000

    Implemented basis for React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,12 @@
-import React, { Component } from 'react';
-import Collection from './components/Collection'
+import React from 'react';
+import Main from './Main';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            currentCollection: []
-        };
-    }
-
-    componentDidMount() {
-        fetch('http://localhost:5000/book')
-            .then(res => {
-                return res.json()
-            }).then(json => {
-
-                let books = json.slice(0, 10);
-                console.log(books);
-                this.setState({ currentCollection: books });
-
-            });
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <h1>ReadRecommend</h1>
-                <Collection currentCollection={this.state.currentCollection} />
-            </div>
-        );
-    }
+function App() {
+    return (
+        <div className="App" >
+            <Main />
+        </div>
+    );
 }
 
 export default App;
