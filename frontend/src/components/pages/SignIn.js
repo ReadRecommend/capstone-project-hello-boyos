df0d2d6387561893afe6df82e6477ca54d36d626
Josh Nila <z5162819@student.unsw.edu.au>
2020-06-29 11:33:24 +1000

    Added React sign in and sign out pages

---
+++ frontend/src/components/pages/SignIn.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import './SignIn.css'
+
+function SignIn() {
+	return (
+		<React.Fragment>
+			<h1>Sign In</h1>
+			<p>This is the page for signing in.</p>
+			<form>
+				<input
+					type="text"
+					name="username"
+					placeholder="Username"
+				/>
+				<input
+					type="password"
+					name="password"
+					placeholder="Password"
+				/>
+				<input
+					type="submit"
+					value="Sign In"
+					className="btn"
+				/>
+			</form>
+		</React.Fragment>
+	)
+}
+
+export default SignIn;
