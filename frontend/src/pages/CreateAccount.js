d964fcae627fe12db44218d384759746b1b1ee21
Felix Lonergan <felix.lonergan@gmail.com>
2020-06-30 21:00:52 +1000

    Improve errors when creating an account with existing username/email

--- frontend/src/pages/CreateAccount.js
+++ frontend/src/pages/CreateAccount.js
@@ -1,102 +1,122 @@
-import React, { Component } from 'react';
-import { Alert } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Alert } from "react-bootstrap";
 
-import './Login.css'
+import "./Login.css";
 
 class CreateAccount extends Component {
-	constructor(props) {
-		super(props)
-		
-		this.state = {
-			username: '',
-			email: '',
-			password: '',
-			errorShow: false,
-			errorMessage: ''
-		};
-	}
+    constructor(props) {
+        super(props);
 
-	updateUsername = (event) => {
-		this.setState({ username: event.target.value });
-		console.log(this.state.username)
-	}
+        this.state = {
+            username: "",
+            email: "",
+            password: "",
+            errorShow: false,
+            errorMessage: "",
+        };
+    }
 
-	updateEmail = (event) => {
-		this.setState({ email: event.target.value });
-	}
+    updateUsername = (event) => {
+        this.setState({ username: event.target.value });
+        console.log(this.state.username);
+    };
 
-	updatePassword = (event) => {
-		this.setState({ password: event.target.value });
-	}
+    updateEmail = (event) => {
+        this.setState({ email: event.target.value });
+    };
 
-	updatePasswordConfirm = (event) => {
-		this.setState({ password_confirm: event.target.value });
-	}
+    updatePassword = (event) => {
+        this.setState({ password: event.target.value });
+    };
 
-	// Function that makes the error not show
-	handleError() {
-		this.setState({ errorShow: false, errorMessage: "" })
-	};
+    updatePasswordConfirm = (event) => {
+        this.setState({ password_confirm: event.target.value });
+    };
 
-	handleSubmit = (event) => {
-		event.preventDefault();
-		if (!this.state.username || !this.state.email || !this.state.password) {
-			this.setState({ errorShow: true, errorMessage: "Please fill in the required fields" });
-			return;
-		}
+    // Function that makes the error not show
+    handleError() {
+        this.setState({ errorShow: false, errorMessage: "" });
+    }
 
-		const data = { username: this.state.username, email: this.state.email, password: this.state.password }
-		fetch('http://localhost:5000/createaccount', {
-			method: 'POST',
-			body: JSON.stringify(data),
-			headers: {
-				'Content-type': 'application/json; charset=UTF-8'
-			}
-		}).then(res => {
-			if (!res.ok) {
-				return res.text().then(text => { throw Error(text) });
-			}
-			return res.json()
-		}).then(json => {
-			// Change route to login
-			return this.props.history.push("/login");
-		});
-	}
+    handleSubmit = (event) => {
+        event.preventDefault();
+        if (!this.state.username || !this.state.email || !this.state.password) {
+            this.setState({
+                errorShow: true,
+                errorMessage: "Please fill in the required fields",
+            });
+            return;
+        }
 
-	render() {
-		return (
-			<div className="CreateAccount">
+        const data = {
+            username: this.state.username,
+            email: this.state.email,
+            password: this.state.password,
+        };
+        fetch("http://localhost:5000/createaccount", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        })
+            .then((res) => {
+                if (!res.ok) {
+                    this.setState({
+                        errorShow: true,
+                        errorMessage:
+                            "This username or password is currently in use. Please choose another",
+                    }).then(() => {
+                        throw Error;
+                    });
+                }
+                return res.json();
+            })
+            .then(() => {
+                // Change route to login
+                return this.props.history.push("/login");
+            })
+            .catch((e) => {});
+    };
 
-				{/* Alert for general problems */}
-				<Alert show={this.state.errorShow} onClose={() => this.handleError()} variant="danger" dismissible>
-					{this.state.errorMessage}
-				</Alert>
+    render() {
+        return (
+            <div className="CreateAccount">
+                {/* Alert for general problems */}
+                <Alert
+                    show={this.state.errorShow}
+                    onClose={() => this.handleError()}
+                    variant="danger"
+                    dismissible
+                >
+                    {this.state.errorMessage}
+                </Alert>
 
-				<h1>Create Account</h1>
-				<p>This is the page for creating an account.</p>
-				<form method="POST" onSubmit={this.handleSubmit}>
-					<input
-						type="text"
-						name="username"
-						placeholder="Username"
-						value={this.state.username}
-						onChange={this.updateUsername}
-					/>
-					<input
-						type="text"
-						name="email"
-						placeholder="Email"
-						value={this.state.email}
-						onChange={this.updateEmail}
-					/>
-					<input
-						type="password"
-						name="password"
-						placeholder="Password"
-						value={this.state.password}
-						onChange={this.updatePassword}
-					/>
-					{/* For confirming password (add later)
+                <h1>Create Account</h1>
+                <p>This is the page for creating an account.</p>
+                <form method="POST" onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="Username"
+                        value={this.state.username}
+                        onChange={this.updateUsername}
+                    />
+                    <input
+                        type="text"
+                        name="email"
+                        placeholder="Email"
+                        value={this.state.email}
+                        onChange={this.updateEmail}
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={this.state.password}
+                        onChange={this.updatePassword}
+                    />
+                    {/* For confirming password (add later)
 					<input
 						type="password"
 						name="password-confim"
@@ -104,15 +124,15 @@ class CreateAccount extends Component {
 						value={this.state.password_confirm}
 						onChange={this.updatePasswordConfirm}
 					/>*/}
-					<input
-						type="submit"
-						value="Create Account"
-						className="btn"
-					/>
-				</form>
-			</div>
-		)
-	}
+                    <input
+                        type="submit"
+                        value="Create Account"
+                        className="btn"
+                    />
+                </form>
+            </div>
+        );
+    }
 }
 
 export default CreateAccount;
