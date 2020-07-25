11927013f19d1f96fde6d478994d47e35e0b6764
Josh Nila <joshnila@Caroles-MBP.hub>
2020-07-25 13:34:34 +1000

    Added recommendation tab with dropdown to select mode

--- frontend/src/pages/BookPage.js
+++ frontend/src/pages/BookPage.js
@@ -2,161 +2,220 @@ import React, { Component } from "react";
 import { getBook } from "../fetchFunctions";
 import AddBookModal from "../components/AddBookModal";
 
-import { Container, Row, Media, Tabs, Tab, Image, Button } from "react-bootstrap";
+import {
+    Container,
+    Row,
+    Media,
+    Tabs,
+    Tab,
+    Image,
+    Button,
+    Form,
+    InputGroup
+} from "react-bootstrap";
 import StarRatings from "react-star-ratings";
 import ReviewList from "../components/ReviewList";
 import AddReview from "../components/AddReview";
 import { toast, ToastContainer } from "react-toastify";
 
 class BookPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      book: {},
-      collection: {},
-    };
-  }
-
-  componentDidMount() {
-    // Fetch the book based on the url
-    getBook(this.props.match.params.bookID)
-      .then((res) => {
-        if (!res.ok) {
-          // Something went wrong, likely there is no book with the id specified in the url
-          return res.text().then((text) => {
-            throw Error(text);
-          });
-        }
-
-        // Found a valid book
-        return res.json();
-      })
-      .then((json) => {
-        this.setState({
-          book: json,
-        });
-      });
-  }
-
-  sortAuthors = (authors) => {
-    return authors.sort(function (a, b) {
-      if (a.includes("(") && !b.includes("(")) {
-        return 1e9;
-      } else if (b.includes("(") && !a.includes("(")) {
-        return -1e9;
-      } else {
-        return a.localeCompare(b);
-      }
-    });
-  };
-
-  notify = (message) => {
-    toast.info(message);
-  };
-
-  render() {
-    const book = this.state.book;
-    const user = this.props.initialUserInfo;
-    if (!book.authors) {
-      return null;
+    constructor(props) {
+        super(props);
+        this.state = {
+            book: {},
+            collection: {},
+            recommendationMode: "Author"
+        };
     }
-    return (
-      <div>
-        <ToastContainer autoClose={4000} pauseOnHover closeOnClick />
-        <Container>
-          <Row>
-            <br></br>
-          </Row>
-          <Row>
-            <Media>
-              <Image
-                className="mr-3"
-                src={book.cover}
-                alt={book.title}
-                thumbnail
-                width="314px"
-              />
-              <Media.Body>
-                <h1>{book.title}</h1>
-                <h5>
-                  <small>{this.sortAuthors(book.authors).join(", ")}</small>
-                </h5>
-                <p>
-                  {user ? (
-                    <AddBookModal
-                      book={book}
-                      user={user}
-                      notify={this.notify}
-                    />
-                  ) : null}
-                  <Button
-                    variant="primary"
-                    href="/recommend"
-                  >
-                    Recommend Similar
-                  </Button>
-                </p>
-                <Tabs defaultActiveKey="summary">
-                  <Tab eventKey="summary" title="Summary">
-                    <br></br>
 
-                    <p>{book.summary}</p>
-                  </Tab>
-                  <Tab eventKey="reviews" title="Reviews + Ratings">
-                    {this.props.initialUserInfo && (
-                      <>
+    componentDidMount() {
+        // Fetch the book based on the url
+        getBook(this.props.match.params.bookID)
+            .then((res) => {
+                if (!res.ok) {
+                    // Something went wrong, likely there is no book with the id specified in the url
+                    return res.text().then((text) => {
+                        throw Error(text);
+                    });
+                }
+
+                // Found a valid book
+                return res.json();
+            })
+            .then((json) => {
+                this.setState({
+                    book: json,
+                });
+            });
+    }
+
+    sortAuthors = (authors) => {
+        return authors.sort(function (a, b) {
+            if (a.includes("(") && !b.includes("(")) {
+                return 1e9;
+            } else if (b.includes("(") && !a.includes("(")) {
+                return -1e9;
+            } else {
+                return a.localeCompare(b);
+            }
+        });
+    };
+
+    notify = (message) => {
+        toast.info(message);
+    };
+
+    handleSubmit = (event) => {
+        // IMPLEMENT LATER
+    }
+
+    render() {
+        const book = this.state.book;
+        const user = this.props.initialUserInfo;
+        if (!book.authors) {
+            return null;
+        }
+        return (
+            <div>
+                <ToastContainer autoClose={4000} pauseOnHover closeOnClick />
+                <Container>
+                    <Row>
                         <br></br>
-                        <AddReview
-                          bookID={this.props.match.params.bookID}
-                          readerID={this.props.initialUserInfo.id}
-                          notify={this.notify}
-                        />
-                      </>
-                    )}
-                    <br></br>
-                    <h5>Average Rating</h5>
-                    <StarRatings
-                      rating={book.ave_rating}
-                      starRatedColor="gold"
-                      numberOfStars={5}
-                      starDimension="30px"
-                      name="rating"
-                    />
-                    <br></br>
-                    <small>
-                      {book.ave_rating} from {book.n_ratings.toLocaleString()}{" "}
-                      reviews
-                    </small>
+                    </Row>
+                    <Row>
+                        <Media>
+                            <Image
+                                className="mr-3"
+                                src={book.cover}
+                                alt={book.title}
+                                thumbnail
+                                width="314px"
+                            />
+                            <Media.Body>
+                                <h1>{book.title}</h1>
+                                <h5>
+                                    <small>
+                                        {this.sortAuthors(book.authors).join(
+                                            ", "
+                                        )}
+                                    </small>
+                                </h5>
+                                <p>
+                                    {user ? (
+                                        <AddBookModal
+                                            book={book}
+                                            user={user}
+                                            notify={this.notify}
+                                        />
+                                    ) : null}
+                                </p>
+                                <Tabs defaultActiveKey="summary">
+                                    <Tab eventKey="summary" title="Summary">
+                                        <br></br>
 
-                    <ReviewList bookID={this.props.match.params.bookID} />
-                  </Tab>
-                  <Tab eventKey="info" title="Additional Information">
+                                        <p>{book.summary}</p>
+                                    </Tab>
+                                    <Tab
+                                        eventKey="reviews"
+                                        title="Reviews + Ratings"
+                                    >
+                                        {this.props.initialUserInfo && (
+                                            <>
+                                                <br></br>
+                                                <AddReview
+                                                    bookID={
+                                                        this.props.match.params
+                                                            .bookID
+                                                    }
+                                                    readerID={
+                                                        this.props
+                                                            .initialUserInfo.id
+                                                    }
+                                                    notify={this.notify}
+                                                />
+                                            </>
+                                        )}
+                                        <br></br>
+                                        <h5>Average Rating</h5>
+                                        <StarRatings
+                                            rating={book.ave_rating}
+                                            starRatedColor="gold"
+                                            numberOfStars={5}
+                                            starDimension="30px"
+                                            name="rating"
+                                        />
+                                        <br></br>
+                                        <small>
+                                            {book.ave_rating} from{" "}
+                                            {book.n_ratings.toLocaleString()}{" "}
+                                            reviews
+                                        </small>
+
+                                        <ReviewList
+                                            bookID={
+                                                this.props.match.params.bookID
+                                            }
+                                        />
+                                    </Tab>
+                                    <Tab
+                                        eventKey="info"
+                                        title="Additional Information"
+                                    >
+                                        <br></br>
+                                        <strong>Publisher: </strong>
+                                        {book.publisher}
+                                        <br></br>
+                                        <strong>Publication Year: </strong>
+                                        {book.publication_date}
+                                        <br></br>
+                                        <strong>Genres: </strong>
+                                        {book.genres.sort().join(", ")}
+                                        <br></br>
+                                        <strong>Language: </strong>
+                                        {book.language}
+                                        <br></br>
+                                        <strong>ISBN: </strong>
+                                        {book.isbn}
+                                        <br></br>
+                                    </Tab>
+                                    <Tab
+                                        eventKey="recommend"
+                                        title="Recommend Similar"
+                                    >
+                                        <br></br>
+                                        <Form
+                                            method="POST"
+                                            onSubmit={this.handleSubmit}
+                                        >
+                                            <InputGroup>
+                                                <Form.Control
+                                                    as="select"
+                                                    defaultValue={"Author"}
+                                                    onChange={this.updateFilter}
+                                                >
+                                                    <option>Author</option>
+                                                    <option>Genre</option>
+                                                </Form.Control>
+                                                <Button
+                                                    variant="primary"
+                                                    type="submit"
+                                                    block
+                                                    value="Recommend"
+                                                >
+                                                    Recommend
+                                                </Button>
+                                            </InputGroup>
+                                        </Form>
+                                    </Tab>
+                                </Tabs>
+                            </Media.Body>
+                        </Media>
+                    </Row>
                     <br></br>
-                    <strong>Publisher: </strong>
-                    {book.publisher}
-                    <br></br>
-                    <strong>Publication Year: </strong>
-                    {book.publication_date}
-                    <br></br>
-                    <strong>Genres: </strong>
-                    {book.genres.sort().join(", ")}
-                    <br></br>
-                    <strong>Language: </strong>
-                    {book.language}
-                    <br></br>
-                    <strong>ISBN: </strong>
-                    {book.isbn}
-                    <br></br>
-                  </Tab>
-                </Tabs>
-              </Media.Body>
-            </Media>
-          </Row>
-          <br></br>
-        </Container>
-      </div>
-    );
-  }
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default BookPage;
