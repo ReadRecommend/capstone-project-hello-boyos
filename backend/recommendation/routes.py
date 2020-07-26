85c766028462583191161a7a4e1e5a50e869d8a8
William Liu <z5205505@ad.unsw.edu.au>
2020-07-26 15:12:45 +1000

    Updated recommendation routes to use sort_books. Added checks to input.

--- backend/recommendation/routes.py
+++ backend/recommendation/routes.py
@@ -10,12 +10,23 @@ from backend.recommendation.content_recommender import ContentRecommender
 @recommendation_bp.route("/author", methods=["POST"])
 def get_author():
 
-    userID = request.json.get("userID")
+    user_id = request.json.get("userID")
+    if not user_id.isdigit():
+        raise InvalidRequest("the userID must be parseable as an integer")
+
+    reader = Reader.query.filter_by(id=user_id).first()
+    if not reader:
+        raise ResourceNotFound("A user with the specified ID does not exist")
+
+    userBooks = sort_books(reader)
+
     # Author NAME
     authorName = request.json.get("author")
+    authorBooks = Author.query.filter_by(name=authorName).first()
 
-    authorBooks = Author.query.filter_by(name=authorName).first().books
-    userBooks = Collection.query.filter_by(reader_id=userID, name="Main").first().books
+    if not authorBooks:
+        raise ResourceNotFound("An author with this name does not exist")
+    authorBooks = authorBooks.books
 
     unreadBooks = list(set(authorBooks) - set(userBooks))
 
@@ -25,12 +36,23 @@ def get_author():
 @recommendation_bp.route("/genre", methods=["POST"])
 def get_genre():
 
-    userID = request.json.get("userID")
+    user_id = request.json.get("userID")
+
+    if not user_id.isdigit():
+        raise InvalidRequest("the userID must be parseable as an integer")
+
+    reader = Reader.query.filter_by(id=user_id).first()
+    if not reader:
+        raise ResourceNotFound("A user with the specified ID does not exist")
+
+    userBooks = sort_books(reader)
+
     # Genre NAME
     genre = request.json.get("genre")
-
-    genreBooks = Genre.query.filter_by(name=genre).first().books
-    userBooks = Collection.query.filter_by(reader_id=userID, name="Main").first().books
+    genreBooks = Genre.query.filter_by(name=genre).first()
+    if not genreBooks:
+        raise ResourceNotFound("A genre with this name does not exist")
+    genreBooks = genreBooks.books
 
     unreadBooks = list(set(genreBooks) - set(userBooks))
 
@@ -40,19 +62,29 @@ def get_genre():
 @recommendation_bp.route("/following", methods=["POST"])
 def get_following():
 
-    userID = request.json.get("userID")
+    user_id = request.json.get("userID")
 
-    reader = Reader.query.filter_by(id=userID).first().follows
+    if not user_id.isdigit():
+        raise InvalidRequest("the userID must be parseable as an integer")
+
+    reader = Reader.query.filter_by(id=user_id).first()
+    if not reader:
+        raise ResourceNotFound("A user with the specified ID does not exist")
+
+    userBooks = sort_books(reader)
+
+    reader = Reader.query.filter_by(id=user_id).first().follows
+    if not reader:
+        raise ResourceNotFound(
+            "The user with the specified ID does not follow any users"
+        )
 
     followingBooks = []
 
-    for item in reader:
-        for collection in item.collections:
-            if collection.name == "Main":
-                followingBooks = followingBooks + collection.books
+    for following in reader:
+        followingBooks = followingBooks + sort_books(following)
 
     followingBooks = list(dict.fromkeys(followingBooks))
-    userBooks = Collection.query.filter_by(reader_id=userID, name="Main").first().books
 
     unreadBooks = list(set(followingBooks) - set(userBooks))
 
