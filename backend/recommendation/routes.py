e8a542496a0cf361bfd37ac38cc4a8c794c56e09
William Liu <z5205505@ad.unsw.edu.au>
2020-07-29 00:25:45 +1000

    A userID for genre/author rec is now optional.

--- backend/recommendation/routes.py
+++ backend/recommendation/routes.py
@@ -11,56 +11,106 @@ from backend.user.utils import sort_books
 @recommendation_bp.route("/author", methods=["POST"])
 def get_author():
 
-    user_id = validate_integer(request.json.get("userID"), "userID")
-
-    reader = Reader.query.filter_by(id=user_id).first()
-    if not reader:
-        raise ResourceNotFound("A user with the specified ID does not exist")
-
     n_recommend = validate_integer(request.json.get("nRecommend", 10), "nRecommend")
 
-    user_books = sort_books(reader)
+    # Seed Book
+    book_id = request.json.get("bookID")
+    book_id = validate_integer(book_id, "bookID")
+
+    book = Book.query.filter_by(id=book_id).first()
+    if not book:
+        raise ResourceNotFound("A book with this ID does not exist")
 
     # Author NAME
-    author_name = request.json.get("author")
-    author_books = Author.query.filter_by(name=author_name).first()
+    if request.json.get("author"):
+        author_name = request.json.get("author")
+        author_books = Author.query.filter_by(name=author_name).first()
+        if not author_books:
+            raise ResourceNotFound("An author with this name does not exist")
+        author_books = author_books.books
+        if book in author_books:
+            author_books.remove(book)
+    else:
+        # If no author name provided:
+        authors = book.authors
+        author_books = []
+        for author in authors:
+            author_name = author.name
+            author_book = Author.query.filter_by(name=author_name).first()
+            if not author_book:
+                raise ResourceNotFound("An author with this name does not exist")
+            author_book = author_book.books
+            author_books = author_books + author_book
+        if book in author_books:
+            author_books.remove(book)
 
-    if not author_books:
-        raise ResourceNotFound("An author with this name does not exist")
-    author_books = author_books.books
+    return_books = sorted(author_books, key=lambda book: book.ave_rating)[:n_recommend]
 
-    unread_books = sorted(
-        list(set(author_books) - set(user_books)), key=lambda book: book.ave_rating
-    )[:n_recommend]
+    if request.json.get("userID"):
+        user_id = validate_integer(request.json.get("userID"), "userID")
+        reader = Reader.query.filter_by(id=user_id).first()
+        if not reader:
+            raise ResourceNotFound("A user with the specified ID does not exist")
+        user_books = sort_books(reader)
+        return_books = sorted(
+            list(set(return_books) - set(user_books)), key=lambda book: book.ave_rating
+        )[:n_recommend]
 
-    return jsonify(books_schema.dump(unread_books))
+    return jsonify(books_schema.dump(return_books))
 
 
 @recommendation_bp.route("/genre", methods=["POST"])
 def get_genre():
 
-    user_id = validate_integer(request.json.get("userID"), "userID")
-
-    reader = Reader.query.filter_by(id=user_id).first()
-    if not reader:
-        raise ResourceNotFound("A user with the specified ID does not exist")
-
     n_recommend = validate_integer(request.json.get("nRecommend", 10), "nRecommend")
 
-    user_books = sort_books(reader)
+    # Seed Book
+    book_id = request.json.get("bookID")
+    book_id = validate_integer(book_id, "bookID")
+
+    book = Book.query.filter_by(id=book_id).first()
+    if not book:
+        raise ResourceNotFound("A book with this ID does not exist")
 
     # Genre NAME
-    genre = request.json.get("genre")
-    genre_books = Genre.query.filter_by(name=genre).first()
-    if not genre_books:
-        raise ResourceNotFound("A genre with this name does not exist")
-    genre_books = genre_books.books
+    if request.json.get("genre"):
+        genre = request.json.get("genre")
+        genre_books = Genre.query.filter_by(name=genre).first()
+        if not genre_books:
+            raise ResourceNotFound("A genre with this name does not exist")
 
-    unread_books = sorted(
-        list(set(genre_books) - set(user_books)), key=lambda book: book.ave_rating
-    )[:n_recommend]
+        genre_books = genre_books.books
+        if book in genre_books:
+            genre_books.remove(book)
+    else:
+        # If genre name provided:
+        genres = book.genres
+        genre_books = []
 
-    return jsonify(books_schema.dump(unread_books))
+        for genre in genres:
+            genre = genre.name
+            genre_book = Genre.query.filter_by(name=genre).first()
+            if not genre_book:
+                raise ResourceNotFound("A genre with this name does not exist")
+            genre_book = genre_book.books
+            genre_books = genre_book + genre_book
+
+        if book in genre_books:
+            genre_books.remove(book)
+
+    return_books = sorted(genre_books, key=lambda book: book.ave_rating)[:n_recommend]
+
+    if request.json.get("userID"):
+        user_id = validate_integer(request.json.get("userID"), "userID")
+        reader = Reader.query.filter_by(id=user_id).first()
+        if not reader:
+            raise ResourceNotFound("A user with the specified ID does not exist")
+        user_books = sort_books(reader)
+        return_books = sorted(
+            list(set(return_books) - set(user_books)), key=lambda book: book.ave_rating
+        )[:n_recommend]
+
+    return jsonify(books_schema.dump(return_books))
 
 
 @recommendation_bp.route("/following", methods=["POST"])
@@ -122,9 +172,10 @@ def get_content():
 
     return jsonify(books_schema.dump(unread_recommendations))
 
+
 @recommendation_bp.route("/toprated", methods=["GET"])
 def get_top():
-    
+
     n_recommend = validate_integer(request.json.get("nRecommend", 10), "nRecommend")
     books = Book.query.order_by(Book.ave_rating.desc()).limit(n_recommend).all()
     return jsonify(books_schema.dump(books))
