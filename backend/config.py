ab8bfbd6cd6531befc0ed2e53fa954610b03ccf7
Felix Lonergan <felix.lonergan@gmail.com>
2020-07-31 16:37:53 +1000

    Clean up configuration

--- backend/config.py
+++ backend/config.py
@@ -4,15 +4,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = (
-        os.getenv("SECRET_KEY") or b"\x06!~\x8c5\x8d\xe8g\x13B\xba\xf2\x1d\xc8\x1d\xa0"
-    )
-    SQLALCHEMY_DATABASE_URI = (
-        os.getenv("DATABASE_URL") or "postgresql://postgres:test123@localhost/test"
+    SECRET_KEY = os.getenv("SECRET_KEY", "A32C30240F4E3248A6A447C71E81785A")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL", "postgresql://postgres:test123@localhost/test"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CORS_HEADERS = "Content-Type"
-    JWT_ACCESS_LIFESPAN = os.getenv("JWT_ACCESS_LIFESPAN") or {"minutes": 30}
-    JWT_REFRESH_LIFESPAN = os.getenv("JWT_REFRESH_LIFESPAN") or {"days": 7}
-    JWT_COOKIE_NAME = os.getenv("JWT_COOKIE_NAME") or "accessToken"
-    JWT_PLACES = os.getenv("JWT_PLACES") or ["cookie", "header"]
+    JWT_ACCESS_LIFESPAN = {"minutes": int(os.getenv("JWT_ACCESS_MINUTES", "30"))}
+    JWT_REFRESH_LIFESPAN = {"days": int(os.getenv("JWT_REFRESH_DAYS", "7"))}
+    JWT_COOKIE_NAME = os.getenv("JWT_COOKIE_NAME", "accessToken")
+    JWT_PLACES = os.getenv("JWT_PLACES", "cookie,header").split(",")
